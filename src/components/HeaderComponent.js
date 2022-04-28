ecb03ced58c5f4bb76f7618775a97bbe6b7388e3
Kiara Richardson <kiararich722@icloud.com>
2022-04-28 13:08:40 -0400

    initial commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Herb &#38; Flow</h2>
+      </>
+    );
+  }
+}
+
+export default Header;
