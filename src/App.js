721ded72a106fe5846cc41f553ce715f7b5d6cbf
Kiara Richardson <kiararich722@icloud.com>
2022-04-28 20:01:04 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from "react";
 import Header from "./components/HeaderComponent";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Blog = () => (
+  <div>
+    <h2>Blog</h2>
+  </div>
+);
+
+const Contact = () => (
+  <div>
+    <h2>Contact</h2>
+  </div>
+);
 
 function App() {
   return (
