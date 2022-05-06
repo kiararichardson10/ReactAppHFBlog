09050d39727066cf96ec7e4e9edd6aed8f38689c
Kiara Richardson <kiararich722@icloud.com>
2022-05-06 14:54:15 -0400

    reactstrap, fonts and CSS

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Header extends React.Component {
   render() {
     return (
       <>
-        <h2>Herb &#38; Flow</h2>
+        <h2>Herb &#38; Fire</h2>
+
+        <main className="px-3">
+          <h1>Welcome to our Garden</h1>
+          <p className="lead" id="coverslogan">
+            An ode to our favorite hobby
+          </p>
+          <p className="lead" id="coverbutton">
+            <Button outline color="warning" href="#">
+              Explore
+            </Button>
+          </p>
+        </main>
       </>
     );
   }
