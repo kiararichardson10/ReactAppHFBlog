fc1cb1d1a358d4a4ebb112e8a58b211c4cff6000
Kiara Richardson <kiararich722@icloud.com>
2022-05-06 15:36:02 -0400

    styling updates

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,19 +6,25 @@ class Header extends React.Component {
   render() {
     return (
       <>
-        <h2>Herb &#38; Fire</h2>
+        <body className="d-flex h-100 bg-image">
+          <div className="d-flex w-100 h-100 p-3 mx-auto flex-column" id="mask">
+            <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+              <h2>Herb &#38; Fire</h2>
 
-        <main className="px-3">
-          <h1>Welcome to our Garden</h1>
-          <p className="lead" id="coverslogan">
-            An ode to our favorite hobby
-          </p>
-          <p className="lead" id="coverbutton">
-            <Button outline color="warning" href="#">
-              Explore
-            </Button>
-          </p>
-        </main>
+              <main className="px-3">
+                <h1>Welcome to our Garden</h1>
+                <p className="lead" id="coverslogan">
+                  A Community of Hobby Growers
+                </p>
+                <p className="lead" id="coverbutton">
+                  <Button outline color="warning" href="#">
+                    Explore
+                  </Button>
+                </p>
+              </main>
+            </div>
+          </div>
+        </body>
       </>
     );
   }
