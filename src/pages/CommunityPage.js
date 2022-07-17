8363f27e569934fe4753b878450d9dc879b3aba9
Kiara Richardson <kiararich722@icloud.com>
2022-07-17 14:07:18 -0400

    Community Page updates

--- src/pages/CommunityPage.js
+++ src/pages/CommunityPage.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { Header } from "../components/Header";
+import {
+  MDBBreadcrumb,
+  MDBBreadcrumbItem,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+} from "mdbreact";
+import { Link } from "react-router-dom";
 
 export function Community() {
   return (
     <>
       <Header />
-      <div>Community Page</div>
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol>
+            <MDBBreadcrumb style={{ backgroundColor: "transparent" }}>
+              <MDBBreadcrumbItem>
+                <Link to="/">Home</Link>
+                {""}
+              </MDBBreadcrumbItem>
+              <MDBBreadcrumbItem active>Community</MDBBreadcrumbItem>
+            </MDBBreadcrumb>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </>
   );
 }
